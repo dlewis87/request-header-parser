73330de5ff6d0e569811d18f5c1311d76783208c
Danny Lewis <dan_e12@hotmai.com>
2016-05-02 12:18:24 +0000

    Finished app

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,11 +2,27 @@
 
 var path = process.cwd();
 
+function createHeader(ip, lang, software){
+	return {"ipaddress":ip,"language":lang,"software":software}
+}
+
 
 module.exports = function (app) {
 	
 	app.get('/',function (req, res) {
-		res.send("Hello World!");
+		
+		var ip = req.headers['x-forwarded-for'] || 
+		     req.connection.remoteAddress || 
+		     req.socket.remoteAddress ||
+		     req.connection.socket.remoteAddress;
+		     
+		var lang = req.headers["accept-language"].split(",")[0];
+		var userAgent = req.headers['user-agent'];
+		var software = userAgent.match(/\([^\)]*/g)[0].replace("(","");
+		
+		res.setHeader('Content-Type', 'application/json');
+		res.json(createHeader(ip, lang, software));
+		
 	});
 	
 	
